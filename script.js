cd5e9cec9a4ca06807e94e1a67fe76dcec28e4e5
zlevin1228 <zacharylevin1228@gmail.com>
2024-01-26 15:00:00 -0500

    added java for two sections and changed css slightly

--- script.js
+++ script.js
@@ -1,23 +1,89 @@
 function year1612() {
-    document.getElementById('')
+    document.body.style.backgroundImage = "url('Images/space.jpg')";
+    let element = document.getElementById("parallax");
+    element.style.backgroundImage = "url('Images/space.jpg')";
+    element.style.backgroundRepeat = "no-repeat";
+    element.style.backgroundPosition = "center";
+    document.getElementById('response-header-1').innerText = 'DECEMBER 28, 1612'
+    document.getElementById('response-header-2').innerText = 'Galileo observes Neptune'
+    document.getElementById('response-text-1').innerText = 'Neptune is too dim to be visible to the naked eye: its apparent magnitude is never brighter than 7.7. Therefore, the first observations of Neptune were only possible after the invention of the telescope. There is evidence that Neptune was seen and recorded by Galileo Galilei in 1613, but he was not known to have recognized it as a planet at the time. These pre-discovery observations were important in accurately determining the orbit of Neptune. Neptune would appear prominently even in early telescopes so other pre-discovery observation records are likely.'
+    document.getElementById('response-img-1').src = 'Images/galileo.jpg'
+    document.getElementById('response-text-2').innerText = 'Galileos drawings show that he observed Neptune on December 28, 1612, and again on January 27, 1613; on both occasions, Galileo mistook Neptune for a fixed star when it appeared very close (in conjunction) to Jupiter in the night sky. Historically it was thought that he believed it to be a fixed blue star, and so he is not credited with its discovery. At the time of his first observation in December 1612, it was stationary in the sky because it had just turned retrograde that very day; because it was only beginning its yearly retrograde cycle, Neptunes motion was thought to be too slight, and its apparent size too small, to clearly appear to be a planet in Galileos small telescope.'
+    document.getElementById('response-img-2').src = 'Images/neptune.jpg'
+    document.getElementById('response-img-3').src = 'Images/galileotelescope.jpg'
 }
 
 function year1832() {
-    document.getElementById('')
+    document.body.style.backgroundImage = "url('Images/flag.jpg')";
+    let element = document.getElementById("parallax");
+    element.style.backgroundImage = "url('Images/flag.jpg')";
+    element.style.backgroundRepeat = "no-repeat";
+    element.style.backgroundPosition = "center";
+    document.getElementById('response-header-1').innerText = 'DECEMBER 28, 1832'
+    document.getElementById('response-header-2').innerText = 'John C. Calhoun Resigns'
+    document.getElementById('response-text-1').innerText = 'Citing political differences with President Andrew Jackson and a desire to fill a vacant Senate seat in South Carolina, John C. Calhoun becomes the first vice president in U.S. history to resign the office. He does this on DEcember 28, 1832. He was a popular political figure in the early 1800s.'
+    document.getElementById('response-img-1').src = 'Images/johnccalhoun.jpg'
+    document.getElementById('response-text-2').innerText = 'Born near Abbeville, South Carolina, in 1782, Calhoun was an advocate of states rights and a defender of the agrarian South against the industrial North. Calhoun served as secretary of war under President James Monroe and in 1824 ran for the presidency. However, bitter partisan attacks from other contenders forced him out of the race, and he had to settle for the vice presidency under President John Quincy Adams.'
+    document.getElementById('response-img-2').src = 'Images/johnccalhoun2.jpg'
+    document.getElementById('response-img-3').src = 'Images/letter.jpg'
 }
 
 function year1922() {
-    document.getElementById('')
+    document.body.style.backgroundImage = "url('Images/space.jpg')";
+    let element = document.getElementById("parallax");
+    element.style.backgroundImage = "url('Images/space.jpg')";
+    element.style.backgroundRepeat = "no-repeat";
+    element.style.backgroundPosition = "center";
+    document.getElementById('response-header-1').innerText = ''
+    document.getElementById('response-header-2').innerText = ''
+    document.getElementById('response-text-1').innerText = ''
+    document.getElementById('response-img-1').src = ''
+    document.getElementById('response-text-2').innerText = ''
+    document.getElementById('response-img-2').src = ''
+    document.getElementById('response-img-3').src = ''
 }
 
 function year1954() {
-    document.getElementById('')
+    document.body.style.backgroundImage = "url('Images/space.jpg')";
+    let element = document.getElementById("parallax");
+    element.style.backgroundImage = "url('Images/space.jpg')";
+    element.style.backgroundRepeat = "no-repeat";
+    element.style.backgroundPosition = "center";
+    document.getElementById('response-header-1').innerText = ''
+    document.getElementById('response-header-2').innerText = ''
+    document.getElementById('response-text-1').innerText = ''
+    document.getElementById('response-img-1').src = ''
+    document.getElementById('response-text-2').innerText = ''
+    document.getElementById('response-img-2').src = ''
+    document.getElementById('response-img-3').src = ''
 }
 
 function year2007() {
-    document.getElementById('')
+    document.body.style.backgroundImage = "url('Images/space.jpg')";
+    let element = document.getElementById("parallax");
+    element.style.backgroundImage = "url('Images/space.jpg')";
+    element.style.backgroundRepeat = "no-repeat";
+    element.style.backgroundPosition = "center";
+    document.getElementById('response-header-1').innerText = ''
+    document.getElementById('response-header-2').innerText = ''
+    document.getElementById('response-text-1').innerText = ''
+    document.getElementById('response-img-1').src = ''
+    document.getElementById('response-text-2').innerText = ''
+    document.getElementById('response-img-2').src = ''
+    document.getElementById('response-img-3').src = ''
 }
 
 function year2008() {
-    document.getElementById('')
+    document.body.style.backgroundImage = "url('Images/space.jpg')";
+    let element = document.getElementById("parallax");
+    element.style.backgroundImage = "url('Images/space.jpg')";
+    element.style.backgroundRepeat = "no-repeat";
+    element.style.backgroundPosition = "center";
+    document.getElementById('response-header-1').innerText = ''
+    document.getElementById('response-header-2').innerText = ''
+    document.getElementById('response-text-1').innerText = ''
+    document.getElementById('response-img-1').src = ''
+    document.getElementById('response-text-2').innerText = ''
+    document.getElementById('response-img-2').src = ''
+    document.getElementById('response-img-3').src = ''
 }
