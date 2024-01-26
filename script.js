335f4c88a4bcf133ff82bef70c8f4b3cfdb349b9
zlevin1228 <zacharylevin1228@gmail.com>
2024-01-26 14:28:19 -0500

    prepared html for java and linked it to java

---
+++ script.js
@@ -0,0 +1,23 @@
+function year1612() {
+    document.getElementById('')
+}
+
+function year1832() {
+    document.getElementById('')
+}
+
+function year1922() {
+    document.getElementById('')
+}
+
+function year1954() {
+    document.getElementById('')
+}
+
+function year2007() {
+    document.getElementById('')
+}
+
+function year2008() {
+    document.getElementById('')
+}
