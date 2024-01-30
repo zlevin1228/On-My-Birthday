a9779ce32d83224df9b5d37d28a6274e453ed42c
zlevin1228 <zacharylevin1228@gmail.com>
2024-01-30 13:50:04 -0500

    added 2 sections

--- script.js
+++ script.js
@@ -7,10 +7,11 @@ function year1612() {
     document.getElementById('response-header-1').innerText = 'DECEMBER 28, 1612'
     document.getElementById('response-header-2').innerText = 'Galileo observes Neptune'
     document.getElementById('response-text-1').innerText = 'Neptune is too dim to be visible to the naked eye: its apparent magnitude is never brighter than 7.7. Therefore, the first observations of Neptune were only possible after the invention of the telescope. There is evidence that Neptune was seen and recorded by Galileo Galilei in 1613, but he was not known to have recognized it as a planet at the time. These pre-discovery observations were important in accurately determining the orbit of Neptune. Neptune would appear prominently even in early telescopes so other pre-discovery observation records are likely.'
-    document.getElementById('response-img-1').src = 'Images/galileo.jpg'
+    document.getElementById('response-img-1').src = 'Images/galileo.jpg' // Img from Wiki
     document.getElementById('response-text-2').innerText = 'Galileos drawings show that he observed Neptune on December 28, 1612, and again on January 27, 1613; on both occasions, Galileo mistook Neptune for a fixed star when it appeared very close (in conjunction) to Jupiter in the night sky. Historically it was thought that he believed it to be a fixed blue star, and so he is not credited with its discovery. At the time of his first observation in December 1612, it was stationary in the sky because it had just turned retrograde that very day; because it was only beginning its yearly retrograde cycle, Neptunes motion was thought to be too slight, and its apparent size too small, to clearly appear to be a planet in Galileos small telescope.'
-    document.getElementById('response-img-2').src = 'Images/neptune.jpg'
-    document.getElementById('response-img-3').src = 'Images/galileotelescope.jpg'
+    document.getElementById('response-img-2').src = 'Images/neptune.jpg' // Img from Google
+    document.getElementById('response-img-3').src = 'Images/galileotelescope.jpg' // Img from unknown website
+    window.scrollTo(0, 0);
 }
 
 function year1832() {
@@ -22,40 +23,43 @@ function year1832() {
     document.getElementById('response-header-1').innerText = 'DECEMBER 28, 1832'
     document.getElementById('response-header-2').innerText = 'John C. Calhoun Resigns'
     document.getElementById('response-text-1').innerText = 'Citing political differences with President Andrew Jackson and a desire to fill a vacant Senate seat in South Carolina, John C. Calhoun becomes the first vice president in U.S. history to resign the office. He does this on DEcember 28, 1832. He was a popular political figure in the early 1800s.'
-    document.getElementById('response-img-1').src = 'Images/johnccalhoun.jpg'
+    document.getElementById('response-img-1').src = 'Images/johnccalhoun.jpg' // Img from History.Com
     document.getElementById('response-text-2').innerText = 'Born near Abbeville, South Carolina, in 1782, Calhoun was an advocate of states rights and a defender of the agrarian South against the industrial North. Calhoun served as secretary of war under President James Monroe and in 1824 ran for the presidency. However, bitter partisan attacks from other contenders forced him out of the race, and he had to settle for the vice presidency under President John Quincy Adams.'
-    document.getElementById('response-img-2').src = 'Images/johnccalhoun2.jpg'
-    document.getElementById('response-img-3').src = 'Images/letter.jpg'
+    document.getElementById('response-img-2').src = 'Images/johnccalhoun2.jpg' // Img from History.Com
+    document.getElementById('response-img-3').src = 'Images/letter.jpg' // Img from Wiki
+    window.scrollTo(0, 0);
 }
 
 function year1922() {
-    document.body.style.backgroundImage = "url('Images/space.jpg')";
+    document.body.style.backgroundImage = "url('Images/marvelbg.jpg')"; // Image from Marvel
     let element = document.getElementById("parallax");
-    element.style.backgroundImage = "url('Images/space.jpg')";
+    element.style.backgroundImage = "url('Images/marvelbg.jpg')";
     element.style.backgroundRepeat = "no-repeat";
     element.style.backgroundPosition = "center";
-    document.getElementById('response-header-1').innerText = ''
-    document.getElementById('response-header-2').innerText = ''
-    document.getElementById('response-text-1').innerText = ''
-    document.getElementById('response-img-1').src = ''
-    document.getElementById('response-text-2').innerText = ''
-    document.getElementById('response-img-2').src = ''
-    document.getElementById('response-img-3').src = ''
+    document.getElementById('response-header-1').innerText = 'DECEMBER 28, 1922'
+    document.getElementById('response-header-2').innerText = 'Stan Lee is born'
+    document.getElementById('response-text-1').innerText = 'Stan Lee is being recognized for his innovations that revolutionized American comic books. Stan Lee created, co-created, and recreated hundreds of superhero characters, most notably Spider-Man, The Incredible Hulk, X-Men, and Captain America. Throughout his career he has devoted himself to the excellence, growth, and availability of this unique American art form.'
+    document.getElementById('response-img-1').src = 'Images/stanlee.jpg' // Image from News Site
+    document.getElementById('response-text-2').innerText = 'Stan Lees career in the comic book industry extends over six decades. Within months of being hired, he was promoted to editor. He has also held the titles of Marvels Head Writer, Art Director, Editor-in-Chief, Publisher, President and Chairman. In 2001, Lee formed POW! Entertainment, and is developing and producing numerous projects for TV, film, and videogames.'
+    document.getElementById('response-img-2').src = 'Images/avengerscameo.jpg' // Image from Avengers Movie
+    document.getElementById('response-img-3').src = 'Images/stanleecharacters.jpg' // Image from Comic site
+    window.scrollTo(0, 0);
 }
 
 function year1954() {
-    document.body.style.backgroundImage = "url('Images/space.jpg')";
+    document.body.style.backgroundImage = "url('Images/rememberthetitans.jpg')";
     let element = document.getElementById("parallax");
-    element.style.backgroundImage = "url('Images/space.jpg')";
+    element.style.backgroundImage = "url('Images/rememberthetitans.jpg')"; // Image from Disney
     element.style.backgroundRepeat = "no-repeat";
     element.style.backgroundPosition = "center";
-    document.getElementById('response-header-1').innerText = ''
-    document.getElementById('response-header-2').innerText = ''
-    document.getElementById('response-text-1').innerText = ''
-    document.getElementById('response-img-1').src = ''
-    document.getElementById('response-text-2').innerText = ''
-    document.getElementById('response-img-2').src = ''
-    document.getElementById('response-img-3').src = ''
+    document.getElementById('response-header-1').innerText = 'DECEMBER 28, 1922'
+    document.getElementById('response-header-2').innerText = 'Denzel Washington is born '
+    document.getElementById('response-text-1').innerText = 'Denzel Hayes Washington, Jr. was born on December 28, 1954 in Mount Vernon, New York. He is the middle of three children of a beautician mother, Lennis, from Georgia, and a Pentecostal minister father, Denzel Washington, Sr., from Virginia. After graduating from high school, Denzel enrolled at Fordham University, intent on a career in journalism.'
+    document.getElementById('response-img-1').src = 'Images/denzelwashington.jpg' // Img from Wiki
+    document.getElementById('response-text-2').innerText = 'Washington has received much critical acclaim for his film work since the 1990s, including his portrayals of real-life figures such as South African anti-apartheid activist Steve Biko in Cry Freedom (1987), Muslim minister and human rights activist Malcolm X in Malcolm X (1992), boxer Rubin "Hurricane" Carter in The Hurricane (1999), football coach Herman Boone in Remember the Titans (2000), poet and educator Melvin B. Tolson in The Great Debaters (2007), and drug kingpin Frank Lucas in American Gangster (2007).'
+    document.getElementById('response-img-2').src = 'Images/theequalizer.jpg' //Img from the Equalizer
+    document.getElementById('response-img-3').src = 'Images/acting.jpg' // Image from Wiki
+    window.scrollTo(0, 0);
 }
 
 function year2007() {
@@ -71,6 +75,7 @@ function year2007() {
     document.getElementById('response-text-2').innerText = ''
     document.getElementById('response-img-2').src = ''
     document.getElementById('response-img-3').src = ''
+    window.scrollTo(0, 0);
 }
 
 function year2008() {
@@ -86,4 +91,5 @@ function year2008() {
     document.getElementById('response-text-2').innerText = ''
     document.getElementById('response-img-2').src = ''
     document.getElementById('response-img-3').src = ''
+    window.scrollTo(0, 0);
 }
